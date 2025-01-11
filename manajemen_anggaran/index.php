508fc2e42313ddf0216c7ad4070cf01bc09f9801
Panggah Widiandana <117331216+panggah10@users.noreply.github.com>
2025-01-11 22:03:11 +0700

    Debugging
    Fitur Manajemen Anggaran

--- manajemen_anggaran/index.php
+++ manajemen_anggaran/index.php
@@ -5,11 +5,11 @@ include '../template/sidebar.php';
 <main id="main" class="main">
 
     <div class="pagetitle">
-        <h1>Dashboard</h1>
+        <h1>Manajemen Anggaran</h1>
         <nav>
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                <li class="breadcrumb-item active">Dashboard</li>
+                <li class="breadcrumb-item"><a href="<?php echo BASE_URL; ?>/sikeu/index.php">Home</a></li>
+                <li class="breadcrumb-item active">Manajemen Anggaran</li>
             </ol>
         </nav>
     </div><!-- End Page Title -->
