a7a766c702f563c9db268c85087d5ba855f77409
Panggah Widiandana <117331216+panggah10@users.noreply.github.com>
2025-01-17 10:25:12 +0700

    Menambah Fitur
    Menambah Fitur Arus Kas

---
+++ arus kas/index.php
@@ -0,0 +1,28 @@
+<?php
+include '../template/header.php';
+include '../template/sidebar.php';
+?>
+<main id="main" class="main">
+
+    <div class="pagetitle">
+        <h1>Arus Kas</h1>
+        <nav>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="<?php echo BASE_URL; ?>/sikeu/index.php">Home</a></li>
+                <li class="breadcrumb-item active">Arus Kas</li>
+            </ol>
+        </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section dashboard">
+        <div class="row">
+
+
+        </div>
+    </section>
+
+</main><!-- End #main -->
+
+<?php
+include '../template/footer.php';
+?>
