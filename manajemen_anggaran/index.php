2987d02733304a1dcd70be39edb0a7c5771bdad9
azam1412 <140772601+azam1412@users.noreply.github.com>
2025-01-29 22:48:14 +0700

    tambah fitur manajemen anggaran

--- manajemen_anggaran/index.php
+++ manajemen_anggaran/index.php
@@ -12,6 +12,65 @@ include '../template/sidebar.php';
                 <li class="breadcrumb-item active">Manajemen Anggaran</li>
             </ol>
         </nav>
+    </div>
+    <div class="container mt-5">
+        <h1 class="text-center"></h1>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Nama Anggaran</th>
+                    <th>Jumlah Anggaran</th>
+                    <th>Tanggal Mulai</th>
+                    <th>Tanggal Berakhir</th>
+                    <th>Kategori</th>
+                    <th>Deskripsi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!--data akan diisi dari database-->
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+
+        </table>
+
+    </div>
+    <div class="container mt-5">
+        <h2 class="text-center">Tambah Anggaran</h2>
+        <form action="">
+            <div class="mb-3">
+                <label for="namaAnggaran" class="form-label">Nama Anggaran</label>
+                <input type="text" class="form-control" id="namaAnggaran" required>
+            </div>
+            <div class="mb-3">
+                <label for="jumlahAnggaran" class="form-label">Jumlah Anggaran</label>
+                <input type="number" class="form-control" id="jumlahAnggaran" required>
+            </div>
+            <div class="mb-3">
+                <label for="tanggalMulai" class="form-label">Tanggal Mulai</label>
+                <input type="date" class="form-control" id="tanggalMulai" required>
+            </div>
+            <div class="mb-3">
+                <label for="tanggalBerakhir" class="form-label">Tanggal Berkahir</label>
+                <input type="date" class="form-control" id="tanggalBerakhir" required>
+            </div>
+            <div class="mb-3">
+                <label for="kategori" class="form-label">Kategori</label>
+                <input type="text" class="form-control" id="kategori" required>
+            </div>
+            <div class="mb-3">
+                <label for="deskripsi" class="form-label">Deskripsi</label>
+                <textarea class="form-control" id="deskripsi" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Simpan</button>
+        </form>
+    
     </div><!-- End Page Title -->
 
     <section class="section dashboard">
@@ -19,6 +78,7 @@ include '../template/sidebar.php';
 
 
         </div>
+
     </section>
 
 </main><!-- End #main -->
